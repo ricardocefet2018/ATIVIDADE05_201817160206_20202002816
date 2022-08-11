505e2f8caf6964f95451081c5e68b66af5d64d41
Ricardo Rocha <ricardocefet2018@gmail.com>
2022-08-11 20:03:10 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ATIVIDADE05_201817160206_20202002816',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
