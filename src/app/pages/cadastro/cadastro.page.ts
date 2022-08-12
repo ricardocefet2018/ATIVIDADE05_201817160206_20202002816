a99b9097d9e9168a6788f9b3050cecb8a1d16ded
Ricardo Rocha <ricardocefet2018@gmail.com>
2022-08-11 23:36:51 -0300

    cadastro page done

--- src/app/pages/cadastro/cadastro.page.ts
+++ src/app/pages/cadastro/cadastro.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage implements OnInit {
+  cadastroForm = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.min(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  submitForm() {
+    if (this.cadastroForm.valid) {
+      this.usuarioService
+        .cadastro(this.cadastroForm.getRawValue())
+        .then((user) => {
+          if (user.id) {
+            this.router.navigate(['/login']);
+          }
+        });
+    } else {
+      this.cadastroForm.markAllAsTouched();
+    }
   }
-
 }
